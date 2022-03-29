548d96da5117cd9b182f4a712ebaca1397b4cc9f
raulgodii <raulgodii13@gmail.com>
2022-03-29 10:27:20 +0200

    Update

---
+++ Practica2/Ejercicio7/main.c
@@ -0,0 +1,6 @@
+/*
+7. Implementa una función que permita multiplicar matrices dinámicas. Utiliza las funciones
+incluidas en la biblioteca libMatrices.a para implementar el programa que te permita probar la
+función.
+*/
+
