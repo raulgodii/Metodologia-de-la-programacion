5c5fd8ce36b1812a22dcdaacafb86a66b54aab37
raulgodii <raulgodii13@gmail.com>
2022-05-15 20:18:38 +0200

    Update

---
+++ Practica3/Ejercicio6/main.c
@@ -0,0 +1,14 @@
+/*
+6. Codifica un programa en C que, utilizando funciones, lea números enteros desde un fichero
+binario generado en el ejercicio anterior, almacene sus valores en un vector dinámico y calcule la
+media de los números pares. El nombre del fichero se le preguntará al usuario.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "prototipos.h"
+
+int main(){
+    
+    return 0;
+}
---
+++ Practica3/Ejercicio6/prototipos.h
@@ -0,0 +1,5 @@
+#ifndef funciones
+#define funciones
+
+
+#endif
